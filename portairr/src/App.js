1a872c0f79beecf5dda047454eaf1eade250be11
Irving Rocha <irvingrocharesendiz@gmail.com>
2025-01-17 01:03:32 -0600

    initial commit

---
+++ portairr/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import HeaderName from './headername';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hi</div>
+      <HeaderName/>
+    </div>
+  );
+}
+
+export default App;
---
+++ portairr/src/headername.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./headername.css";
+
+export default function HeaderName() {
+    return (
+        <div className="header">
+            <h1>👋🏻 Hola, Soy Irving Rocha Reséndiz</h1>
+            <div>👍🏻 Me gusta: </div>
+            <ul>
+                <li>Programar Videojuegos 🎮</li>
+                <li>Páginas Web 💻 </li>
+                <li>Aplicaciones móviles 📱 </li>
+                <li>Diseñar e imprimir modelos 3D 🛠️</li>
+            </ul>
+        </div>)
+}
